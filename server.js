4dd3200e5ccf664a8340adfaa2b6291e99b221ef
Satendra Kumar Diwakar <sadevexe@gmail.com>
2021-08-06 21:56:56 +0530

    create wikiDB with mongodb

--- server.js
+++ server.js
@@ -1,26 +1,61 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-const mongoose= require('mongoose');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get('/',(req,res)=>{
-    res.send("Hello from REST API");
+mongoose.connect('mongodb://localhost:27017/wikiDB', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const wikiSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Please mention title']
+    },
+    content: String
 });
+const WikiArticles = mongoose.model("Wiki", wikiSchema);
 
-app.post('/',(req,res)=>{
-    res.send("Post on route \" / \" ");
+const wikiOne = new WikiArticles({
+    title: "API",
+    content: "API stands for Application Programming Interface. It is a set of subroutine definitions, communication protocols, and tools for building software. In general terms, it is a set of clearly defined methods of communication among various components. A good API makes it easier to develop a computer program by providing all the building blocks, which are then put together by the programmer."
+})
+
+const wikiTwo = new WikiArticles({
+    title: "Bootstrap",
+    content: "This is a framework developed by Twitter that contains pre-made front-end templates for web design"
 })
 
 
-
-app.listen(3000,()=>{
-    console.log('app listen on port 3000');
+const wikiThree = new WikiArticles({
+    title: "DOM",
+    content: "The Document Object Model is like an API for interacting with our HTML"
 })
+
+// app.get('/', (req, res) => {
+//     res.send("Hello from REST API");
+// });
+
+// app.post('/', (req, res) => {
+//     res.send("Post on route \" / \" ");
+// })
+
+// WikiArticles.insertMany([wikiOne, wikiTwo, wikiThree], err => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log('Success');
+//         mongoose.connection.close();
+//     }
+// });
+
+
+// app.listen(3000, () => {
+//     console.log('app listen on port 3000');
+// })
