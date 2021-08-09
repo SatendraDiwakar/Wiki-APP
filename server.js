fbb8a521955794e9a5c0f0452356f7b57c5676f7
Satendra Kumar Diwakar <sadevexe@gmail.com>
2021-08-10 01:10:36 +0530

    Build front-end v1 , add delete functionality

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const path = require('path');
 
 const app = express();
 
-app.set('views', './views');
+app.set('views', './views')
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,9 +22,48 @@ const wikiSchema = new mongoose.Schema({
 });
 const WikiArticles = mongoose.model("Wiki", wikiSchema);
 
+const article1 = new WikiArticles({
+    title: 'ReactJs',
+    content: 'React is a free and open-source front-end JavaScript library for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications.'
+});
 
-app.get('/',(req,res)=>{
-    res.render('list', {listTitle: 'hello'});
+const article2 = new WikiArticles({
+    title: 'HTML',
+    content: 'The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript.'
+});
+
+const article3 = new WikiArticles({
+    title: 'CSS',
+    content: 'Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.'
+});
+
+const defaultArticles = [article1, article2, article3];
+
+
+app.get('/', (req, res) => {
+    WikiArticles.find((err, foundItems) => {
+        if (foundItems.length === 0) {
+            WikiArticles.insertMany(defaultArticles, err => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Successfully savevd default items to DB.");
+                }
+            });
+            res.redirect("/");
+        } else {
+            res.render("list", { listItems: foundItems });
+        }
+    });
+});
+app.post('/delete/:id', (req,res)=>{
+
+    WikiArticles.findByIdAndRemove(req.params.id, err => {
+        if (!err) {
+          console.log("Successfully deleted checked item.");
+          res.redirect("/");
+        }
+      });
 });
 // chained route handling in docs
 // if we have different http request for same route
@@ -73,64 +111,64 @@ app.route('/articles')
 /*******************Request targeting a specific article*******************/
 
 app.route('/articles/:articleTitle')
-.get((req,res)=>{
+    .get((req, res) => {
 
-    // to use route parameters
-    // req.params.articleTitle = 'React';
-    // to get data which has space in url we need to replace with %20
-    // ex - localhost:3000/articles/Context%20API
-    // to know more https://www.w3schools.com/tags/ref_urlencode.ASP
+        // to use route parameters
+        // req.params.articleTitle = 'React';
+        // to get data which has space in url we need to replace with %20
+        // ex - localhost:3000/articles/Context%20API
+        // to know more https://www.w3schools.com/tags/ref_urlencode.ASP
 
-    WikiArticles.findOne({title: req.params.articleTitle},(err,foundArticle)=>{
-        if (err) {
-            res.send("No Articles match");
-        } else {
-            res.send(foundArticle);
-        }
+        WikiArticles.findOne({ title: req.params.articleTitle }, (err, foundArticle) => {
+            if (err) {
+                res.send("No Articles match");
+            } else {
+                res.send(foundArticle);
+            }
+        })
     })
-})
-.put((req,res)=>{
-    // updating whole document 
-    // and if no value is there(req.body) then replaced with null
-    WikiArticles.updateOne(
-        {title: req.params.articleTitle},
-        {title: req.body.title, content: req.body.content},
-        err=>{
-            if (err) {
-                res.send(`Cannot change ${req.params.articleTitle}`)
-            } else {
-                res.send(`Successfully change ${req.params.articleTitle}`)
+    .put((req, res) => {
+        // updating whole document 
+        // and if no value is there(req.body) then replaced with null
+        WikiArticles.updateOne(
+            { title: req.params.articleTitle },
+            { title: req.body.title, content: req.body.content },
+            err => {
+                if (err) {
+                    res.send(`Cannot change ${req.params.articleTitle}`)
+                } else {
+                    res.send(`Successfully change ${req.params.articleTitle}`)
+                }
             }
-        }
-    )
-})
-.patch((req,res)=>{
-    // updating a property of a document
-    // also req.body sends a object what and which fields needed to update
-    WikiArticles.updateOne(
-        {title:req.params.articleTitle},
-        {$set: req.body},
-        err=>{
-            if (err) {
-                res.send(`Cannot change ${req.params.articleTitle}`)
-            } else {
-                res.send(`Successfully change ${req.params.articleTitle}`)
+        )
+    })
+    .patch((req, res) => {
+        // updating a property of a document
+        // also req.body sends a object what and which fields needed to update
+        WikiArticles.updateOne(
+            { title: req.params.articleTitle },
+            { $set: req.body },
+            err => {
+                if (err) {
+                    res.send(`Cannot change ${req.params.articleTitle}`)
+                } else {
+                    res.send(`Successfully change ${req.params.articleTitle}`)
+                }
             }
-        }
-    )
-})
-.delete((req,res)=>{
-    WikiArticles.deleteOne(
-        {title:req.params.articleTitle},
-        err=>{
-            if (err) {
-                res.send(`Cannot delete ${req.params.articleTitle}`)
-            } else {
-                res.send(`Successfully deleted ${req.params.articleTitle}`)
+        )
+    })
+    .delete((req, res) => {
+        WikiArticles.deleteOne(
+            { title: req.params.articleTitle },
+            err => {
+                if (err) {
+                    res.send(`Cannot delete ${req.params.articleTitle}`)
+                } else {
+                    res.send(`Successfully deleted ${req.params.articleTitle}`)
+                }
             }
-        }
-    )
-});
+        )
+    });
 
 app.listen(3000, () => {
     console.log('app listen on port 3000');
